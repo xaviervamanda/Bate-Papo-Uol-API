384b4adf312b7d2cebfb8835055147e606e47c48
Amanda Xavier <amandavxavier@gmail.com>
2023-04-13 12:45:17 -0300

    Requisito: endpoint GET /participants

--- src/app.js
+++ src/app.js
@@ -16,5 +16,16 @@ mongoClient.connect()
     .then(() => db = mongoClient.db())
     .catch(err => console.log(err.message))
 
+app.post ("/participants", (req, res) => {
+    const {name} = req.body;
+    //validar com joi
+})
+
+app.get("/participants", (req, res => {
+    db.collection("participants").find().toArray()
+        .then(participants => res.status(200).send(participants))
+        .catch(err => res.status(500).send(err.message))
+}))
+
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
